736fb7e6cdb0b383e0b9ddab59367e29c104b103
JordanKPark <50196157+JordanKPark@users.noreply.github.com>
2019-05-05 02:40:25 -0500

    Add files via upload

--- ParkASCIISlotMachine/Park_Final/Park_Final.cpp
+++ ParkASCIISlotMachine/Park_Final/Park_Final.cpp
@@ -278,6 +278,25 @@ void seed()
 {
 	srand((unsigned int)time(NULL));
 }
+//**************************************************************************
+//this function sets the window size                                       *
+//**************************************************************************
+void SetWindow(int Width, int Height)
+{
+	_COORD coord;
+	coord.X = Width;
+	coord.Y = Height;
+
+	_SMALL_RECT Rect;
+	Rect.Top = 0;
+	Rect.Left = 0;
+	Rect.Bottom = Height - 1;
+	Rect.Right = Width - 1;
+
+	HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);      // Get Handle 
+	SetConsoleScreenBufferSize(Handle, coord);            // Set Buffer Size 
+	SetConsoleWindowInfo(Handle, TRUE, &Rect);            // Set Window Size 
+}
 //***************************************************************************
 //this function returns a random number and takes in a min and max for the  *
 //the number to be between.													*
@@ -1385,6 +1404,7 @@ void printstats()
 //**************************************************************************
 int main()
 {
+	SetWindow(100, 40);
 	startsc();
 	hdr();
 	seed();
